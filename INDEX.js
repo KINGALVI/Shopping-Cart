ad5594ff3cf4b95652ecf28f604115de6300ddd8
KINGALVI <alvialmahamud@gmail.com>
2023-10-07 00:53:30 +0600

    edit some code

--- INDEX.js
+++ INDEX.js
@@ -1,8 +1,22 @@
 document.getElementById('btn-phone-minus').addEventListener('click', function () {
 
+    const minusPhoneNumberField = document.getElementById('input-phone-field')
+    const minusPhoneNumberString = minusPhoneNumberField.value;
+    const minusCurentPhoneNumber = parseInt(minusPhoneNumberString);
+    const minusPhoneNumber = minusCurentPhoneNumber - 1;
+    const phoneNumberField = minusPhoneNumberField.value = minusPhoneNumber;
+    if(phoneNumberField < 0){
+        minusPhoneNumberField.value = 0;
+    }
 })
 
 document.getElementById('btn-phone-plus').addEventListener('click', function () {
+
+    const plusPhoneNumberField = document.getElementById('input-phone-field')
+    const plusPhoneNumberString = plusPhoneNumberField.value;
+    const plusPriviousPhoneNumber = parseInt(plusPhoneNumberString);
+    const plusNewPhoneNumber = plusPriviousPhoneNumber + 1;
+    plusPhoneNumberField.value = plusNewPhoneNumber;
 
 })
 
