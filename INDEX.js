e51388466c8303f8c3c7fd0fa9d6c1ed5967d166
KINGALVI <alvialmahamud@gmail.com>
2023-10-07 01:24:18 +0600

    edit some code

--- INDEX.js
+++ INDEX.js
@@ -2,10 +2,10 @@ document.getElementById('btn-phone-minus').addEventListener('click', function ()
 
     const minusPhoneNumberField = document.getElementById('input-phone-field')
     const minusPhoneNumberString = minusPhoneNumberField.value;
-    const minusCurentPhoneNumber = parseInt(minusPhoneNumberString);
-    const minusPhoneNumber = minusCurentPhoneNumber - 1;
+    const minusCurrentPhoneNumber = parseInt(minusPhoneNumberString);
+    const minusPhoneNumber = minusCurrentPhoneNumber - 1;
     const phoneNumberField = minusPhoneNumberField.value = minusPhoneNumber;
-    if(phoneNumberField < 0){
+    if (phoneNumberField < 0) {
         minusPhoneNumberField.value = 0;
     }
 })
@@ -22,8 +22,22 @@ document.getElementById('btn-phone-plus').addEventListener('click', function () 
 
 document.getElementById('btn-case-minus').addEventListener('click', function () {
 
+    const minusCaseNumberInputField = document.getElementById('input-case-field');
+    const minusCaseNumberString = minusCaseNumberInputField.value;
+    const minusCurrentCaseNumber = parseInt(minusCaseNumberString);
+    const minusCaseNumber = minusCurrentCaseNumber - 1;
+    const CaseNumberField = minusCaseNumberInputField.value = minusCaseNumber;
+    if (CaseNumberField < 0) {
+        minusCaseNumberInputField.value = 0;
+    }
 })
 
 document.getElementById('btn-case-plus').addEventListener('click', function () {
+
+    const plusCaseNumberField = document.getElementById('input-case-field');
+    const plusCaseNumberFieldString = plusCaseNumberField.value;
+    const plusPriviousCaseNumber = parseInt(plusCaseNumberFieldString);
+    const plusNewCaseNumber = plusPriviousCaseNumber + 1;
+    plusCaseNumberField.value = plusNewCaseNumber;
 
 })
