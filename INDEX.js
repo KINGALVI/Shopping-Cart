377355389d1149586b2eb89b1b1703b7188e0a3f
KINGALVI <alvialmahamud@gmail.com>
2023-10-06 23:35:00 +0600

    edit some code

---
+++ INDEX.js
@@ -0,0 +1,15 @@
+document.getElementById('btn-phone-minus').addEventListener('click', function () {
+
+})
+
+document.getElementById('btn-phone-plus').addEventListener('click', function () {
+
+})
+
+document.getElementById('btn-case-minus').addEventListener('click', function () {
+
+})
+
+document.getElementById('btn-case-plus').addEventListener('click', function () {
+
+})
