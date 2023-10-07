8d9c0d279309d5ce1d19aaf95ca28a2f92873494
KINGALVI <alvialmahamud@gmail.com>
2023-10-07 20:12:56 +0600

    edit some code

--- INDEX.js
+++ INDEX.js
@@ -1,43 +1,71 @@
-document.getElementById('btn-phone-minus').addEventListener('click', function () {
+function phoneNumberPlusMinus(isIncrease) {
 
     const minusPhoneNumberField = document.getElementById('input-phone-field')
     const minusPhoneNumberString = minusPhoneNumberField.value;
     const minusCurrentPhoneNumber = parseInt(minusPhoneNumberString);
-    const minusPhoneNumber = minusCurrentPhoneNumber - 1;
-    const phoneNumberField = minusPhoneNumberField.value = minusPhoneNumber;
-    if (phoneNumberField < 0) {
+    let newPhoneNumber;
+
+    if (isIncrease) {
+        newPhoneNumber = minusCurrentPhoneNumber + 1;
+    }
+    else {
+        newPhoneNumber = minusCurrentPhoneNumber - 1;
+    }
+
+    const PhoneNumberField = minusPhoneNumberField.value = newPhoneNumber;
+    if (PhoneNumberField < 0) {
         minusPhoneNumberField.value = 0;
     }
-})
+
+}
+
 
 document.getElementById('btn-phone-plus').addEventListener('click', function () {
 
-    const plusPhoneNumberField = document.getElementById('input-phone-field')
-    const plusPhoneNumberString = plusPhoneNumberField.value;
-    const plusPriviousPhoneNumber = parseInt(plusPhoneNumberString);
-    const plusNewPhoneNumber = plusPriviousPhoneNumber + 1;
-    plusPhoneNumberField.value = plusNewPhoneNumber;
+    phoneNumberPlusMinus(true)
+
+})
+
+
+document.getElementById('btn-phone-minus').addEventListener('click', function () {
+
+    phoneNumberPlusMinus(false)
+
+})
+
+
+
+
+
+function caseNumberPlusMinus(isIncrease) {
+    const minusCaseNumberInputField = document.getElementById('input-case-field');
+    const minusCaseNumberString = minusCaseNumberInputField.value;
+    const minusCurrentCaseNumber = parseInt(minusCaseNumberString);
+    let nweCaseNumber;
+
+    if (isIncrease === true) {
+        nweCaseNumber = minusCurrentCaseNumber + 1;
+    }
+    else {
+        nweCaseNumber = minusCurrentCaseNumber - 1;
+    }
+
+    const CaseNumberField = minusCaseNumberInputField.value = nweCaseNumber;
+
+    if (CaseNumberField < 0) {
+        minusCaseNumberInputField.value = 0;
+    }
+
+}
+
+document.getElementById('btn-case-plus').addEventListener('click', function () {
+
+    caseNumberPlusMinus(true)
 
 })
 
 document.getElementById('btn-case-minus').addEventListener('click', function () {
 
-    const minusCaseNumberInputField = document.getElementById('input-case-field');
-    const minusCaseNumberString = minusCaseNumberInputField.value;
-    const minusCurrentCaseNumber = parseInt(minusCaseNumberString);
-    const minusCaseNumber = minusCurrentCaseNumber - 1;
-    const CaseNumberField = minusCaseNumberInputField.value = minusCaseNumber;
-    if (CaseNumberField < 0) {
-        minusCaseNumberInputField.value = 0;
-    }
-})
-
-document.getElementById('btn-case-plus').addEventListener('click', function () {
-
-    const plusCaseNumberField = document.getElementById('input-case-field');
-    const plusCaseNumberFieldString = plusCaseNumberField.value;
-    const plusPriviousCaseNumber = parseInt(plusCaseNumberFieldString);
-    const plusNewCaseNumber = plusPriviousCaseNumber + 1;
-    plusCaseNumberField.value = plusNewCaseNumber;
+    caseNumberPlusMinus(false)
 
 })
